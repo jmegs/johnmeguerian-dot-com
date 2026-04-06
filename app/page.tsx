11512a37809e30f42a098666dcc04423e69a49d5
John Meguerian <johnmeguerian@gmail.com>
2026-04-06 19:37:30 -0400

    Add basic layout and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,36 @@
+import Image from "next/image"
+import AvatarImage from "./avatar.png"
+
 export default function Home() {
-	return <p>hi</p>
+	return (
+		<div className="page-grid | p-8 h-screen uppercase text-center lg:text-left">
+			<div id="uno" className="lg:mt-auto">
+				<Image
+					src={AvatarImage}
+					alt="picture of john"
+					loading="eager"
+					className="mx-auto mb-4 lg:m-0 size-16 rounded-full border-4 border-wash"
+				/>
+			</div>
+			<div id="dos">
+				<h1>John Meguerian</h1>
+			</div>
+			<div id="tres">Design Manager</div>
+			<div id="cuatro" className="mt-auto flex flex-col gap-4">
+				<p>
+					johnmeguerian
+					<br />
+					[at] gmail.com
+				</p>
+				<p>
+					Brooklyn
+					<br />
+					New York
+					<br />
+					United States
+				</p>
+				<p>[ 40°40&apos;N, 73°59&apos;W ]</p>
+			</div>
+		</div>
+	)
 }
